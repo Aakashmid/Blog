efe4a936e1ac1e1ff5f3713e5930c68ad2655598
Aakash Kumar Jha <aakash13102005u@gmail.com>
2023-11-13 16:01:33 +0530

    modified

--- blogProject2/settings.py
+++ blogProject2/settings.py
@@ -130,6 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
+
+# managing media
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL="/media/"
